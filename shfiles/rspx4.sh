87206a9cc86886c85db4cf52c1ef96ee21973ebb
shikaison <3464243684@qq.com>
2024-04-02 11:40:59 +0800

    1
    1

--- shfiles/rspx4.sh
+++ shfiles/rspx4.sh
@@ -1,5 +1,5 @@
 sudo chmod 777 /dev/ttyACM0 & sleep 2;
 roslaunch realsense2_camera rs_camera.launch & sleep 10;
 roslaunch mavros px4.launch & sleep 10;
-roslaunch vins fast_drone_250.launch
+roslaunch vins ipac_drone_330.launch
 wait;
